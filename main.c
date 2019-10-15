5a01950b8564b55514b6c7fe0074e821522d78ef
lilly5791 <1913084@sookmyung.ac.kr>
2019-10-15 15:39:50 +0900

    06silsup6

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -7,14 +8,15 @@ int square(int n);
 
 int main(void)
 {
-	int result;
-	result = square(5);
-	printf("%d", result);
+	int i;
+	int max = 45;
+	
+	srand((unsigned)time(NULL));
+	
+	for(i = 0; i < 6; i++)
+		printf("%d ", 1 + rand()%max); // 나머지연산했으므로 (0-44)+1 중 하나 나옴 
+		
+	return 0;	
+	
 }
 
-
-int square(int n) {
-	
-	return(n*n); // square가 return으로 대체됨
-	
-}
