9148b4758f1367a80d8097b957823fada0bd862b
lilly5791 <1913084@sookmyung.ac.kr>
2019-10-21 16:49:37 +0900

    06silsupp2

--- main.c
+++ main.c
@@ -4,36 +4,31 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int calCombination(int n, int r)
+int sumTwo(int a, int b)
 {
-	int high, low;
-	high = factorial(n);
-	low = factorial(r) * factorial(n-r);
-	
-	return (high/low);
-	
+	return (a + b);
 }
 
-int factorial(int a)
+int square(int n)
 {
-	int res = 1, i;
-	for(i = 1; i <= a; i++)
-		res = res * i;	
-	return res;			
+	return (n*n);
 }
 
-
-int main(void)
+int max(int a, int b)
 {
-	int n, r, res;
-	
-	printf("input n & r:");
-	scanf("%d %d", &n, &r);
-	
-	res = calCombination(n, r);
-	printf("%d combination %d = %d", n, r, res);
-
+	if (a > b)
+		return (a);
+	else
+		return (b);	
 }
 
+int main(int argc, char *argv[]) {
+	
+	printf("sum is %d\n", sumTwo(8, 9));
+	printf("square is %d\n", square(8));
+	printf("max is %d\n", max(8, 9));	
+
+	return 0;
+}
 
 
